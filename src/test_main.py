8bccdc71f2837b601a61979dc3a7683590647fe4
Helena Hope-Carter <90761872+helboi4@users.noreply.github.com>
2023-03-09 16:56:03 +0000

    Change assertions to value of index() function return

--- src/test_main.py
+++ src/test_main.py
@@ -10,9 +10,9 @@ def client():
 
 
 def test_hello_world_message_from_index():
-    assert index() == 'Hello World'
+    assert index() == 'Hello there'
 
 
 def test_hello_world_message_from_client(client):
     response = client.get('/')
-    assert response.data.decode('UTF-8') == 'Hello World'
+    assert response.data.decode('UTF-8') == 'Hello there'
